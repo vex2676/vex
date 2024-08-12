6f94dbe59aae9819ec606b984b37c7d034f8fadd
DeanDark <93433446+D3anDark@users.noreply.github.com>
2024-08-12 20:56:34 +0200

    full price has been paid

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("openModal");
+var span = document.getElementsByClassName("close")[0];
+var body = document.body;
+var topElement = document.querySelector(".top");
+
+btn.onclick = function() {
+    modal.style.display = "block";
+    body.classList.add("modal-open");
+    topElement.classList.add("top-hidden");
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+    body.classList.remove("modal-open");
+    topElement.classList.remove("top-hidden");
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        body.classList.remove("modal-open");
+        topElement.classList.remove("top-hidden");
+    }
+}
